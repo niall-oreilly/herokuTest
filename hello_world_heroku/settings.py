bbc51e7432b86f5d1b175b912924490908622d1f
Niall O'Reilly <nialloreilly123@gmail.com>
2016-04-22 10:21:06 +0100

    fixed staticfiles bug

--- hello_world_heroku/settings.py
+++ hello_world_heroku/settings.py
@@ -123,7 +123,7 @@ STATIC_URL = '/static/'
 
 
 STATICFILES_DIRS = [
-    os.path.join(BASE_DIR, 'static'),
+    os.path.join(BASE_DIR, 'staticfiles'),
 ]
 ​
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
